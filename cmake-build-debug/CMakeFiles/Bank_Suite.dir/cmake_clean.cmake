9a52f796de8b3ae20b3f7f4c797364a31c1b8f3a
Almir Redzematovic <almir@iamalmir.tech>
2023-01-25 05:02:51 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/Bank_Suite.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Bank_Suite"
+  "Bank_Suite.pdb"
+  "CMakeFiles/Bank_Suite.dir/main.cpp.o"
+  "CMakeFiles/Bank_Suite.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bank_Suite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Bank_Suite.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Bank_Suite.
+# This may be replaced when dependencies are built.
